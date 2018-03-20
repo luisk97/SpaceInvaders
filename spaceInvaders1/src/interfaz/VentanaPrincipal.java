c4b10fc22f627f661f69415fed7e8498692691b8
luisk <luisk@192.168.1.2>
2018-03-20 02:28:35 -0600

    Pequeños arreglos a la interfaz

--- spaceInvaders1/src/interfaz/VentanaPrincipal.java
+++ spaceInvaders1/src/interfaz/VentanaPrincipal.java
@@ -18,6 +18,7 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 	JButton btnSalir;
 	Image imagenPrueba;
 	JLabel label;
+	JLabel label1;
 	int x = 233;
 	int y = 285;
 	private JButton btnDer;
@@ -33,13 +34,9 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		setBounds(100, 100, 701, 484);
+		setBounds(100, 100, 686, 484);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
-		btnNewButton = new JButton(new ImageIcon("C:/Users/luisk.PC-LUISK/Documents/tec/lll Semestre/Datos l/Proyecto#1/Imagenes/botonStart1.jpg"));
-		btnNewButton.addActionListener(this);
-		btnNewButton.setBounds(278, 162, 113, 116);
-		getContentPane().add(btnNewButton);
 		
 		label = new JLabel(new ImageIcon("C:/Users/luisk.PC-LUISK/Documents/tec/lll Semestre/Datos l/Proyecto#1/Imagenes/navePrincipal3.png"));
 		label.setBounds(x, y, 202, 156);
@@ -53,21 +50,33 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 		getContentPane().add(btnSalir);
 		
 		btnDer = new JButton("der");
-		btnDer.setBounds(657, 248, 28, 23);
+		btnDer.setBounds(643, 248, 28, 23);
 		btnDer.addActionListener(this);
 		btnDer.setVisible(false);
 		getContentPane().add(btnDer);
 		
 		btnIzq = new JButton("izq");
 		btnIzq.addActionListener(this);
-		btnIzq.setBounds(629, 248, 28, 23);
+		btnIzq.setBounds(615, 248, 28, 23);
 		btnIzq.setVisible(false);
 		getContentPane().add(btnIzq);
+		
+		label1 = new JLabel(new ImageIcon("C:/Users/luisk.PC-LUISK/Documents/tec/lll Semestre/Datos l/Proyecto#1/Imagenes/imagenFondo1.jpg"));
+		label1.setBounds(0, 0, 671, 445);
+		label1.setVisible(false);
+		getContentPane().add(label1);
+		
+		btnNewButton = new JButton(new ImageIcon("C:/Users/luisk.PC-LUISK/Documents/tec/lll Semestre/Datos l/Proyecto#1/Imagenes/botonStart1.jpg"));
+		btnNewButton.addActionListener(this);
+		btnNewButton.setBounds(278, 162, 113, 116);
+		getContentPane().add(btnNewButton);
+		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==btnNewButton) {
+			label1.setVisible(true);
 			label.setVisible(true);
 			btnSalir.setVisible(true);
 			btnNewButton.setVisible(false);
@@ -79,16 +88,17 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 			btnNewButton.setVisible(true);
 			btnIzq.setVisible(false);
 			btnDer.setVisible(false);
+			label1.setVisible(false);
 		}else if(e.getSource()==btnDer) {
 			x = x+10;
 			label.setBounds(x, y, 202, 156);
 			getContentPane().add(label);
+			getContentPane().add(label1);
 		}else if(e.getSource()==btnIzq) {
 			x = x-10;
 			label.setBounds(x, y, 202, 156);
 			getContentPane().add(label);
+			getContentPane().add(label1);
 		}
 	}
-	
-	
 }
